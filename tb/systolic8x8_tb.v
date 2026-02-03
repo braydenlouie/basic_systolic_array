6e536cb8e9021c6778cedb7b0e71a40b6c33ead0
braydenlouie <braydenl9988@gmail.com>
2026-02-03 13:12:34 -0500

    fixed 8x8 tb to account for sign

--- tb/systolic8x8_tb.v
+++ tb/systolic8x8_tb.v
@@ -32,19 +32,19 @@ initial begin
         load = 1;
         for (i = 0; i < ARRAY_SIZE; i = i + 1) begin 
             @(negedge clk)
-            weights = {4'd8, 4'd7, 4'd6, 4'd5, 4'd4, 4'd3, 4'd2, 4'd1};
+            weights = {4'd7, -4'd6, 4'd5, -4'd4, 4'd3, 4'd2, 4'd1, 4'd0};
         end
 
         @(negedge clk)
         weights = 0;
         load = 0;
 
-        for (t = 1; t <= ARRAY_SIZE; t = t + 1) begin
+        for (t = 0; t < ARRAY_SIZE; t = t + 1) begin
             @(negedge clk)
             activations = {activations[(ARRAY_SIZE - 1)*DATA_WIDTH - 1 : 0], t[DATA_WIDTH - 1 : 0]};
         end
 
-        for (d = 1; d <= ARRAY_SIZE; d = d + 1) begin
+        for (d = 0; d < ARRAY_SIZE; d = d + 1) begin
             @(negedge clk)
             activations = activations << DATA_WIDTH;
         end
