92d0d3ea7696941c957db4f9d52e3519712fc2d1
Brayden Louie <braydenl9988@gmail.com>
2026-02-04 20:29:39 -0500

    changes for running on Arty S7-25 FPGA
    Signed-off-by: Brayden Louie <braydenl9988@gmail.com>

--- src/systolic_top.v
+++ src/systolic_top.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 
 module systolic_top(
-    input clk_pin
+    input CLK100MHZ
 );
     parameter SIZE = 8;
     parameter WIDTH = 4;
@@ -13,16 +13,24 @@ module systolic_top(
     wire [SIZE * WIDTH * WIDTH - 1 : 0] vio_output;
 
     // Instance of Systolic
-    systolic #(.ARRAY_SIZE(SIZE), .DATA_WIDTH(WIDTH)) core {
-        .clk(clk_pin),
+    systolic #(.ARRAY_SIZE(SIZE), .DATA_WIDTH(WIDTH)) core (
+        .clk(CLK100MHZ),
         .reset(vio_reset),
         .load(vio_load),
         .weights(vio_weights),
         .activations(vio_activations),
-        .output_row(.vio_output)
-    }
+        .output_row(vio_output)
+    );
 
     // Instantiation of VIO IP
+    vio_0 sys_vio (
+        .clk(CLK100MHZ),
+        .probe_in0(vio_output),      // Connects to dashboard
+        .probe_out0(vio_reset),      // Controlled by dashboard
+        .probe_out1(vio_load),
+        .probe_out2(vio_weights),
+        .probe_out3(vio_activations)
+    );
 
     
     
